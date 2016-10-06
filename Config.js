2e261945fbfb3f4e3d213422eb2af2d28daea614
Joona Heikkilä <joona.o.heikkila@gmail.com>
2016-10-06 23:12:32 +0300

    Change default port to 3000

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const path = require('path');
 var Config = {
     DataFile: path.join(__dirname, process.env.DATA_FILE || 'datamme.json'),
     TelegramJoinLink: process.env.TG_JOIN_LINK,
-    Port: process.env.PORT || 80
+    Port: process.env.PORT || 3000
 };
 
 module.exports = Config;
