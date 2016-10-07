8924f2cc4d145b791aa1dd7eb0d584ea592be173
Joona Heikkilä <joona.o.heikkila@gmail.com>
2016-10-07 20:08:22 +0300

    Only use express.static in prod

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const CachedReader = require('./CachedReader.js');
 
 const dataReader = new CachedReader(Config.DataFile, 10); // 5min cache life
 
-app.use(express.static('./client/build'));
+if (process.env.NODE_ENV !== 'production') {
+    // dev mode's frontend is provided by create-react-app's dev server
+    app.use(express.static('./client/build'));
+}
 
 app.get('/api/random', function(req, res) {
     res.setHeader('Cache-Control', 'no-cache'); // no cacherino
